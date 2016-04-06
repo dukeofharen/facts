563bc971106b80231b00a12fb4ec5c6260d066ac
Duco Winterwerp <duco@winterwerp.it>
2016-04-06 20:44:45 +0200

    Extended CategoryManager unit tests

--- Ducode.Facts.Business.Tests/Implementations/CategoryManagerFacts.cs
+++ Ducode.Facts.Business.Tests/Implementations/CategoryManagerFacts.cs
@@ -1,6 +1,8 @@
 ﻿using Ducode.Facts.Business.Implementations;
 using Ducode.Facts.Entities;
+using Ducode.Facts.Exceptions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTestExtensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,7 +18,7 @@ namespace Ducode.Facts.Business.Tests.Implementations
 		[TestMethod]
 		public async Task CategoryManager_GetAll_HappyFlow()
 		{
-			//act
+			//arrange
 			var context = MockContext.CleanInstance;
 			var service = CreateService();
 
@@ -34,13 +36,50 @@ namespace Ducode.Facts.Business.Tests.Implementations
 			context.Add(category1);
 			context.Add(category2);
 
-			//arrange
+			//act
 			var result = await service.GetAll();
 
 			//assert
 			Assert.AreEqual(2, result.Length);
 			Assert.AreEqual(category1.Description, result[0].Description);
 			Assert.AreEqual(category1.Name, result[0].Name);
+		}
+		#endregion
+
+		#region Get
+		[TestMethod]
+		public void CategoryManager_Get_CategoryNotFound()
+		{
+			//arrange
+			var context = MockContext.CleanInstance;
+			var service = CreateService();
+
+			//act / assert
+			ThrowsAsyncAssert.ThrowsAsync<NotFoundException>(service.Get(0));
+		}
+
+		[TestMethod]
+		public async Task CategoryManager_Get_HappyFlow()
+		{
+			//arrange
+			var context = MockContext.CleanInstance;
+			var service = CreateService();
+			var category = new Category()
+			{
+				Id = context.Random(),
+				Description = context.Random().ToString(),
+				Name = context.Random().ToString()
+			};
+			context.Add(category);
+
+			//act
+			var result = await service.Get(category.Id);
+
+			//assert
+			Assert.IsNotNull(result);
+			Assert.AreEqual(category.Id, result.Id);
+			Assert.AreEqual(category.Description, result.Description);
+			Assert.AreEqual(category.Name, result.Name);
 		}
 		#endregion
 
